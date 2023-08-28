463dbefe2bb85564f949163fed796602040dc104
Vu <longvunguyen2003@gmail.com>
2023-08-28 11:45:11 +0700

    ADD NEW VARIABLES AND FUNCTION Add 3 new variables to store 2 operands and 1 operator Add function to do the given math operator with 2 given operands

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function multiply(fisrtNumber, secondNumber){
 function divide(fisrtNumber, secondNumber){
     return fisrtNumber / secondNumber;
 }
+
+//create variables to store value
+let fisrtNumber = 0;
+let secondNumber = 0;
+//create a varible to store operator
+let operator = '+'
+
+//create a function that take an operator and two numbers, return the result
+function operate(fisrtNumber, secondNumber, operator){
+    switch (operator) {
+        case '+':
+            return add(fisrtNumber, secondNumber);
+        case '-': 
+            return subtract(fisrtNumber, secondNumber);
+        case '*':
+            return multiply(fisrtNumber, secondNumber);
+        case '/':
+            return divide(fisrtNumber, secondNumber);
+        default:
+            break;
+    }
+}
