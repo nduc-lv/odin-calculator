1fa1843a63e29fe3a3109e134bb178ec04170d30
Vu <longvunguyen2003@gmail.com>
2023-08-28 11:39:40 +0700

    ADD BASIC FUNCTIONS

---
+++ script.js
@@ -0,0 +1,13 @@
+//create basic math operators
+function add(fisrtNumber, secondNumber){
+    return fisrtNumber + secondNumber;
+}
+function subtract(fisrtNumber, secondNumber){
+    return fisrtNumber - secondNumber;
+}
+function multiply(fisrtNumber, secondNumber){
+    return fisrtNumber * secondNumber;
+}
+function divide(fisrtNumber, secondNumber){
+    return fisrtNumber / secondNumber;
+}
